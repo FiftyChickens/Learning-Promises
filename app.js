fef06bbf8b1cafac2728b10ae45dcb2172f50dc4
Bryan Vu <bryanvu@me.com>
2024-06-26 18:03:09 -0500

    Commented out number facts fetch requests.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-let printFact = document.querySelector('p')
-let factNumbers = '3';
-for (let i = 0; i < 4; i++) {
-    fetch(`http://numbersapi.com/${factNumbers}/?json`)
-        .then(response => response.json())
-        .then(data => {
-            // for (const key in data) {
-            //     printFact.innerText += `${data[key]}
-            // `;
-            // }
-            printFact.innerText += `${data.text}
-            `;
-        })
-        .catch(err => console.log(err))
-}
+// let printFact = document.querySelector('p')
+// let factNumbers = '3';
+// for (let i = 0; i < 4; i++) {
+//     fetch(`http://numbersapi.com/${factNumbers}/?json`)
+//         .then(response => response.json())
+//         .then(data => {
+//             // for (const key in data) {
+//             //     printFact.innerText += `${data[key]}
+//             // `;
+//             // }
+//             printFact.innerText += `${data.text}
+//             `;
+//         })
+//         .catch(err => console.log(err))
+// }
