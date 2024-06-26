f34dcb0509f40a770eb7232488dfeb6a3eb3a3ec
Bryan Vu <bryanvu@me.com>
2024-06-26 14:58:33 -0500

    Made a variable for changing the number the fact is about

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let printFact = document.querySelector('p')
-
-fetch('http://numbersapi.com/3/?json')
+let factNumbers = '1..4';
+fetch(`http://numbersapi.com/${factNumbers}/?json`)
     .then(response => response.json())
     .then(data => printFact.innerText = data.text)
     .catch(err => console.log(err))
