7fba64c5a4dfffe792fd41b4182f6b0bdd0a5d06
Bryan Vu <bryanvu@me.com>
2024-06-26 18:55:27 -0500

    Clicking on button now draws a card till 52 cards are drawn

--- deckApp.js
+++ deckApp.js
@@ -14,7 +14,17 @@
 //         .catch(err => console.log(err))
 // }
 
+let drawn = 1;
 fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/shuffle/?deck_count=1`)
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(err => console.log(err))
+
+const drawButton = document.getElementById('draw');
+drawButton.addEventListener('click', () => {
+    console.log(drawn++);
+    fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/draw/?count=26`)
+        .then(response => response.json())
+        .then(data => console.log(data.cards[0].value, data.cards[0].suit))
+        .catch(err => console.log(err))
+})
