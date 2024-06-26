91a1adaa6a35cefd2f2ef28430809219899b2571
Bryan Vu <bryanvu@me.com>
2024-06-26 15:06:18 -0500

    no longer prints different numbers, now prints 4 facts about a single number to page

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let printFact = document.querySelector('p')
-let factNumbers = '1..4';
-fetch(`http://numbersapi.com/${factNumbers}/?json`)
-    .then(response => response.json())
-    .then(data => {
-        for (const key in data) {
-            printFact.innerText += `${data[key]}
+let factNumbers = '3';
+for (let i = 0; i < 4; i++) {
+    fetch(`http://numbersapi.com/${factNumbers}/?json`)
+        .then(response => response.json())
+        .then(data => {
+            // for (const key in data) {
+            //     printFact.innerText += `${data[key]}
+            // `;
+            // }
+            printFact.innerText += `${data.text}
             `;
-        }
-    })
-    .catch(err => console.log(err))
-
+        })
+        .catch(err => console.log(err))
+}
