3f19aef6c4e38dd4cf2e39303be3803d9cf70d77
Bryan Vu <bryanvu@me.com>
2024-06-26 14:59:23 -0500

    Changed the method of adding facts to page

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ let printFact = document.querySelector('p')
 let factNumbers = '1..4';
 fetch(`http://numbersapi.com/${factNumbers}/?json`)
     .then(response => response.json())
-    .then(data => printFact.innerText = data.text)
+    .then(data => {
+        for (const key in data) {
+            printFact.innerText += `${data[key]}
+            `;
+        }
+    })
     .catch(err => console.log(err))
 
