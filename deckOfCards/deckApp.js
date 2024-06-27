d5d41c2e366b45a74d26c7d7bd9df2d6fe079f9a
Bryan Vu <bryanvu@me.com>
2024-06-26 19:28:50 -0500

    Added folder for drawing Cards removed comments

--- deckOfCards/deckApp.js
+++ deckOfCards/deckApp.js
@@ -1,18 +1,3 @@
-// let printFact = document.querySelector('p')
-// let factNumbers = '3';
-// for (let i = 0; i < 4; i++) {
-//     fetch(`http://numbersapi.com/${factNumbers}/?json`)
-//         .then(response => response.json())
-//         .then(data => {
-//             // for (const key in data) {
-//             //     printFact.innerText += `${data[key]}
-//             // `;
-//             // }
-//             printFact.innerText += `${data.text}
-//             `;
-//         })
-//         .catch(err => console.log(err))
-// }
 
 let drawn = 1;
 fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/shuffle/?deck_count=1`)
