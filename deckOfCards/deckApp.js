ab03073d3cb53516026a923a445482931f6104ec
Bryan Vu <bryanvu@me.com>
2024-06-26 20:37:02 -0500

    Removed card drawn log

--- deckOfCards/deckApp.js
+++ deckOfCards/deckApp.js
@@ -1,13 +1,12 @@
-
-let drawn = 1;
 fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/shuffle/?deck_count=1`)
     .then(response => response.json())
     .then(data => (data))
     .catch(err => console.log(err))
 
 const drawButton = document.getElementById('draw');
+let drawn = 1;
+
 drawButton.addEventListener('click', () => {
-    console.log(drawn++);
     fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/draw/?count=1`)
         .then(response => response.json())
         .then(data => console.log(data.cards[0].value, data.cards[0].suit))
