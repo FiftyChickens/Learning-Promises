dd151b05929fa940b83ca765d989eef6704b4071
Bryan Vu <bryanvu@me.com>
2024-06-26 19:44:05 -0500

    button will now be removed when out of cards

--- deckOfCards/deckApp.js
+++ deckOfCards/deckApp.js
@@ -2,14 +2,19 @@
 let drawn = 1;
 fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/shuffle/?deck_count=1`)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => (data))
     .catch(err => console.log(err))
 
 const drawButton = document.getElementById('draw');
 drawButton.addEventListener('click', () => {
     console.log(drawn++);
-    fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/draw/?count=26`)
+    fetch(`https://deckofcardsapi.com/api/deck/rwsxpwlsd867/draw/?count=1`)
         .then(response => response.json())
         .then(data => console.log(data.cards[0].value, data.cards[0].suit))
         .catch(err => console.log(err))
+
+    if (53 === drawn) {
+        console.log('Out of cards');
+        drawButton.remove();
+    }
 })
